4fc47f5a11cdaadbe4f2decee775f4b15d4474ee
ZeepReactorr <151944715+ZeepReactorr@users.noreply.github.com>
2023-12-15 12:45:56 +0100

    Add files via upload

--- pipeline_2.sh
+++ pipeline_2.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-echo "Guppy is too long to be done here : using pre-made results"
+#Calling the basecaller guppy : 
+
+#Set up the model file for Guppy to treat hereafter based on the sequencing kit that was used
+/PATH/TO/guppy_basecaller --print_workflows 
+
+#Start the actual basecalling step on the binary sequencing files, indicating the flow cell types used
+/PATH/TO/ont-guppy-cpu/bin/guppy_basecaller -i /PATH/TO/fast5 -s /PATH/TO/guppy_out -c dna_r9.4.1_450bps_hac.cfg --num_callers 2 --cpu_threads_per_caller 1
 
 echo "starting trimming"
 
-cdna_classifier.py ~/course_data/dataset/Dmel.4.filt.fastq ~/course_data/pipeline_cdna/pychopper_trimming/output.fastq
+#call to the pychopper trimmer specialized for cDNA trimming. Input : fastq file from the basecaller. output : fastq trimmed file
+cdna_classifier.py /PATH/TO/untrimmed.fastq ~/PATH/TO/trimmed_sequences.fastq
 
 echo "Trimming done"
 
 echo "Read to read mapping with flye"
 
-python ~/Flye/bin/flye --nano-corr ~/course_data/Trimming/pychoppered.fastq --out-dir ~/course_data/assembly/flye/ --threads 4
+#call to the assembler flye to find read to read overlaps. Input : trimmed reads as fastq file. Output : fasta file of assembled reads.
+python ~/Flye/bin/flye --nano-corr ~/PATH/TO/trimmed_sequences.fastq --out-dir ~/PATH/TO/OUTPUT_DIRECTORY/ --threads 4
+
+#error correction step to adjust precision of the assembly
+racon ~/PATH/TO/trimmed_sequences.fastq ~/PATH/TO/alignment.paf ~/PATH/TO/OUTPUT_DIRECTORY/assembly.fasta > ~/PATH/TO/OUTPUT_DIRECTORY/flye_corrected.racon.consensus.fasta
 
 echo "starting clustering with cd-hit"
 
-cd-hit-est -i ~/course_data/cd-hit_test/pychopper/output.fasta -o ~/course_data/cd-hit_test/cd-hit_results/output1.fasta -c 0.9 -n 8 -M 16000 -T 4
+#Clustering module. Input : fastq file from trimming step. Output : fasta file from clustered reads.
+cd-hit-est -i ~/PATH/TO/trimmed_sequences.fastq -o ~/PATH/TO/OUTPUT_DIRECTORY/output.fasta -c 0.9 -n 8 -M 16000 -T 4
 
 echo "alignement try out"
 
-minimap2 -a -x map-ont ~/course_data/Trimming/pychoppered.fastq ~/course_data/assembly/shasta/Assembly.fasta | samtools view -S -b > ~/course_data/alignment/trimmed_reads/mapped_unsorted.bam
+#Alignment of clustered reads on the assembly. Input : clustered reads from cd-hit and assembly from flye. Output : alignment file to viewable in IGV.
+minimap2 -a -x map-ont ~/PATH/TO/clustered_sequences.fasta ~/PATH/TO/assembly.fasta | samtools view -S -b > ~/PATH/TO/OUTPUT_DIRECTORY/mapped_unsorted.bam
 
-racon ~/course_data/Trimming/pychoppered.fastq ~/course_data/assembly/flye/minimap_flye.racon.paf ~/course_data/assembly/flye/assembly.fasta > ~/course_data/error_correction/flye_corrected.racon.consensus.fasta
